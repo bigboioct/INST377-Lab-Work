9c438b7dea2fb5c7f73e60483748e1fc39836617
bigboioct <43962896+bigboioct@users.noreply.github.com>
2023-02-18 00:28:49 -0500

    buttons

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -38,10 +38,16 @@ function moveToNextSlide() {
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
-  if (slidePosition === 0 ){
-    slidePosition = 2;
+  /* if (slidePosition === 0 ){
+    slidePosition = totalSlides - 1;
   } else {
     slidePosition -= 1;
+  };  */
+
+    if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition += 1;
   };
 
   // add your code in here for when you click the "prev" button
