776136c3f151feedc9666c89540add980d3bd2a8
bigboioct <43962896+bigboioct@users.noreply.github.com>
2023-03-11 15:36:24 -0500

    final submission

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -5,6 +5,11 @@
 
 /* A quick filter that will return something based on a matching input */
 function filterList(list, query) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery= query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  })
   /*
     Using the .filter array method, 
     return a list that is filtered by comparing the item name in lower case
@@ -23,30 +28,12 @@ async function mainEvent() { // the async keyword means we can make API requests
   
   /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
   mainForm.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
-  });
 
-  filterButton.addEventListener('click', async (event) => {
-    console.log('clicked FilterButton');
-
-
-  })
-    
-    // This prevents your page from becoming a list of 1000 records from the county, even if your form still has an action set on it
+     // This prevents your page from becoming a list of 1000 records from the county, even if your form still has an action set on it
     submitEvent.preventDefault(); 
-    
+
     // this is substituting for a "breakpoint" - it prints to the browser to tell us we successfully submitted the form
     console.log('form submission'); 
-
-    /*
-      ## GET requests and Javascript
-        We would like to send our GET request so we can control what we do with the results
-        Let's get those form results before sending off our GET request using the Fetch API
-    
-      ## Retrieving information from an API
-        The Fetch API is relatively new,
-        and is much more convenient than previous data handling methods.
-        Here we make a basic GET request to the server using the Fetch method to the county
-    */
 
     // Basic GET request - this replaces the form Action
     const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
@@ -62,19 +49,30 @@ async function mainEvent() { // the async keyword means we can make API requests
     console.table(currentList); 
   });
 
+  filterButton.addEventListener('click', async (event) => {
+    console.log('clicked FilterButton');
 
-  /*
-    Now that you HAVE a list loaded, write an event listener set to your filter button
-    it should use the 'new FormData(target-form)' method to read the contents of your main form
-    and the Object.fromEntries() method to convert that data to an object we can work with
+    const formData = new FormData(mainForm);
+    const formProps = Object.fromEntries(formData);
 
-    When you have the contents of the form, use the placeholder at line 7
-    to write a list filter
+    console.log(formProps);
+    const newList = filterList(currentList, formProps.resto);
+    
+    console.log(newList);
+  })
+    
 
-    Fire it here and filter for the word "pizza"
-    you should get approximately 46 results
-  */
-}
+    /*
+      ## GET requests and Javascript
+        We would like to send our GET request so we can control what we do with the results
+        Let's get those form results before sending off our GET request using the Fetch API
+    
+      ## Retrieving information from an API
+        The Fetch API is relatively new,
+        and is much more convenient than previous data handling methods.
+        Here we make a basic GET request to the server using the Fetch method to the county
+    */
+  }
 
 /*
   This adds an event listener that fires our main event only once our page elements have loaded
